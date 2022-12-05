2af4402e98cff5f2f0f90fb6a854a2df83cfd730
Salvador Borit <salvaborit@gmail.com>
2022-12-05 15:35:30 -0300

    .

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,14 @@ Flask app module
 """
 from flask import Flask
 from models import storage
+from os import getenv
 from api.v1.views import app_views
+HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+PORT = getenv('HBNB_API_PORT', '5000')
 
 
 app = Flask(__name__)
-
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
@@ -19,7 +22,7 @@ def teardown_db(exception):
 
 
 if __name__ == '__main__':
-    from os import getenv
-    app.run(host=getenv('HBNB_API_HOST'),
-            port=getenv('HBNB_API_PORT'),
-            threaded=True)
+    app.run(host=HOST,
+            port=PORT,
+            threaded=True,
+            debug=True)
