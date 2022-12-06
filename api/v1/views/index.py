a7a6c04ffd112ac9d9b12f07b1eab242f58b673d
Salvador Borit <salvaborit@gmail.com>
2022-12-06 18:54:36 -0300

    .

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,10 @@ def status():
 @app_views.route('/stats')
 def stats():
     """ retrieves number of objects by type """
+    from models import storage
+    return dict(amenities=storage.count('Amenity'),
+                cities=storage.count('City'),
+                places=storage.count('Place'),
+                reviews=storage.count('Review'),
+                states=storage.count('State'),
+                users=storage.count('User'))
