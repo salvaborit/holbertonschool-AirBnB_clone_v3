71214cb6d1641ff1c24373d01db61f841e09dda3
Salvador Borit <salvaborit@gmail.com>
2022-12-07 13:42:27 -0300

    .

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
 """
-Module state
+Module states
 """
+from flask import abort, request
 from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route('/states')
+
+@app_views.route('/states', methods=['GET'])
 def get_all_states():
     """ retrieves all states """
-    from models import storage
     states_return = []
     for state in storage.all('State').values():
         states_return.append(state.to_dict())
     return states_return
+
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE'])
+def get_state_by_id(state_id):
+    """ retrieves state by id """
+    for state in storage.all('State').values():
+        if state.id == state_id:
+            if request.method == 'GET':
+                return state.to_dict()
+            elif request.method == 'DELETE':
+                storage.delete(state)
+    abort(404)
--- setup_db_tables.sh
+++ setup_db_tables.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# db setup
+pkill python3
 cat setup_mysql_dev.sql | mysql -uroot -p
-echo "quit" | HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db ./console.py
+
+# # run console with env vars to create tables
+# echo "quit" | HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db ./console.py
+
+
+# run app
 HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db HBNB_API_HOST=0.0.0.0 HBNB_API_PORT=5000 python3 -m api.v1.app
+# sleep 3s
+
+# cat 10-dump.sql | mysql -uroot -p
+
+# self explanatory
