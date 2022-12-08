46ca26e753779e77dc05b09c0c7971b434428cd3
Salvador Borit <salvaborit@gmail.com>
2022-12-07 21:08:54 -0300

    .

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-Module states
+Module state
 """
-from flask import abort, request
 from api.v1.views import app_views
+from flask import abort, request
 from models import storage
+from models.state import State
 
 
-
-@app_views.route('/states')
+@app_views.route('/states', strict_slashes=False, methods=['GET'])
 def get_all_states():
     """ retrieves all states """
     states_return = []
@@ -16,13 +16,47 @@ def get_all_states():
         states_return.append(state.to_dict())
     return states_return
 
-@app_views.route('/states/<state_id>', methods=['GET', 'DELETE'])
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET', 'DELETE'])
 def get_state_by_id(state_id):
-    """ retrieves state by id """
+    """ retrieves state by id for GET, DELETE """
     for state in storage.all('State').values():
         if state.id == state_id:
             if request.method == 'GET':
                 return state.to_dict()
             elif request.method == 'DELETE':
                 storage.delete(state)
+                storage.save()
+                return {}, 200
     abort(404)
+
+@app_views.route('/states', strict_slashes=False, methods=['POST'])
+def post_state():
+    """ creates a state """
+    http_request = request.get_json(silent=True)
+    if http_request is None:
+        abort(400, 'Not a JSON')
+    elif 'name' not in http_request.keys():
+        abort(400, 'Missing name')
+    else:
+        new_state = State(**http_request)
+        storage.new(new_state)
+        storage.save()
+        return new_state.to_dict(), 201
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['PUT'])
+def put_state(state_id):
+    """ updates a state """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    http_request = request.get_json(silent=True)
+    if http_request is None:
+        abort(400, 'Not a JSON')
+    for key, val in http_request.items():
+        if key == 'id' or key == 'created_at' or key == 'updated_at':
+            pass
+        else:
+            setattr(state, key, val)
+    storage.save()
+    return state.to_dict(), 200
--- setup_db_tables.sh
+++ setup_db_tables.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# db setup
 pkill python3
 cat setup_mysql_dev.sql | mysql -uroot -p
 
 # # run console with env vars to create tables and quit
 echo "quit" | HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db HBNB_API_HOST=0.0.0.0 HBNB_API_PORT=5000 python3 -m console
 
-# run app
-HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db HBNB_API_HOST=0.0.0.0 HBNB_API_PORT=5000 python3 -m api.v1.app &
-sleep 3s
+cat insert_all.sql | mysql -uroot -p
 
-cat 10-dump.sql | mysql -uroot -p
-
-# self explanatory
+HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db HBNB_API_HOST=0.0.0.0 HBNB_API_PORT=5000 python3 -m api.v1.app
+# sleep 3s
