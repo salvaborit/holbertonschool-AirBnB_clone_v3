1accbd47ef6ad24c79ec59ef9e698c5b710eadaa
Salvador Borit <salvaborit@gmail.com>
2022-12-08 11:39:53 -0300

    .

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -33,3 +33,37 @@ def get_city(city_id):
         storage.delete(city)
         storage.save()
         return jsonify({}), 200
+
+
+@app_views.route('/states/<state_id>/cities', strict_slashes=False, methods=['POST'])
+def post_city(state_id):
+    """ posts a city obj """
+    req = request.get_json(silent=True)
+    if req is None:
+        abort(400, 'Not a JSON')
+    elif 'name' not in req.keys():
+        abort(400, 'Missing name')
+    else:
+        new_city = City(**req)
+        storage.new(new_city)
+        storage.save()
+        return jsonify(new_city.to_dict()), 201
+
+
+@app_views.route('/cities/<city_id>', strict_slashes=False, methods=['PUT'])
+def put_city(city_id):
+    """ updates a city """
+    req = request.get_json(silent=True)
+    if req is None:
+        abort(400, 'Not a JSON')
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    for key, val in req.items():
+        if key == 'id' or key == 'state_id' or \
+                key == 'created_at' or key == 'updated_at':
+            pass
+        else:
+            setattr(city, key, val)
+    storage.save()
+    return jsonify(city.to_dict()), 200
